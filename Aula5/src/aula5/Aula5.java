7648294c174fb4df531677ab5ca7ff1412a67fe3
Geovana7 <141193054+Geovana7@users.noreply.github.com>
2023-08-14 20:25:09 -0300

    Add files via upload

---
+++ Aula5/src/aula5/Aula5.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula5;
+
+/**
+ *
+ * @author 15671393612
+ */
+public class Aula5 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        String b = "10";
+        int c = 2;
+        String d = "10";
+        
+        int resultadoA = a + c;
+        int resultadoB = a - c;
+        int resultadoC = a * c;
+        int resultadoD = a / c;
+        int resultadoE = b + d;
+        int resultadoF = a % c;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(resultadoA);
+        System.out.println(resultadoF);
+        
+    }
+    
+}
