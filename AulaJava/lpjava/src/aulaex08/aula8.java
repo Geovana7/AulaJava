c4727128f97f676050b194b9943fcf356b6445cc
Geovana7 <141193054+Geovana7@users.noreply.github.com>
2023-08-15 20:15:16 -0300

    Add files via upload

---
+++ AulaJava/lpjava/src/aulaex08/aula8.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulaex08;
+
+/**
+ *
+ * @author 15671393612
+ */
+public class aula8 {
+    
+}
---
+++ AulaJava/lpjava/src/aulaex5/aula05.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aulaex5;
+
+/**
+ * Exercício 05
+ * 
+ * Aula:
+ * Disciplina: Linguagem de Programação
+ * Classe Math: constantes, principais métodos e chamando métodos de outras classes
+ * Documentação 1:https://www.w3schools.com/java/java_math.asp
+ * @author GEOVANA
+ * 
+ */
+public class aula05 {
+    public static void main(String[] args) {
+
+System.out.println("O valor do pi é:" + Math.PI);
+System.out.println("Maximo:" + Math.max(5, 10));
+System.out.println("Minimo:" + Math.min(5, 10));
+System.out.println(Math.sqrt(64));
+
+System.out.println("Exponencial e potenciação\n");
+System.out.println("O valor de E é: " +  Math.E + "\n\n");
+System.out.println("'e' elevado ao quadrado = " + Math.exp(2));
+System.out.println("2 elevado" + Math.pow(2, 3)+"\n\n");
+    }
+}
