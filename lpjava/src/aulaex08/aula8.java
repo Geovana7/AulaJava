ef04e315c4ba5d7a90cee9ccfc539b356e645816
Geovana7 <141193054+Geovana7@users.noreply.github.com>
2023-08-15 22:19:04 -0300

    Add files via upload

--- lpjava/src/aulaex08/aula8.java
+++ lpjava/src/aulaex08/aula8.java
@@ -4,10 +4,25 @@
  */
 package aulaex08;
 
-/**
- *
- * @author 15671393612
+/**Exercício 08
+ * Linguagem de Programação
+ *Materia: Operadores Lógicos e Estruturas Condicionais
+ * @author Geovana
  */
 public class aula8 {
-    
+    public static void main(String[] args) {
+     int a = 10;
+     int b = 10;
+     
+     System.out.println(a > b && a < b);
+     if(a > b){
+         System.out.println("Maior");
+     }else if(a < b){
+         System.out.println("Menor");
+     }else{
+         System.out.println("Igual");
+         
+     }
+    }
+ 
 }
